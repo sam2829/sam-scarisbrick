a799282c5cb79dae26fa7bee77a8a422989bdd88
sam2829 <samscarisbrick@outlook.com>
2024-10-12 11:57:03 +0100

    Add urls for projects

--- my_portfolio/projects/filters.py
+++ my_portfolio/projects/filters.py
@@ -3,6 +3,9 @@ from .models import Project
 
 
 class ProjectFilter(django_filters.FilterSet):
+    """
+    class to filter projects by technology name
+    """
     technology_name = django_filters.CharFilter(field_name='technologies__name', lookup_expr='iexact')
 
     class Meta:
--- my_portfolio/projects/views.py
+++ my_portfolio/projects/views.py
@@ -4,9 +4,21 @@ from .serializers import ProjectSerializer
 from .filters import ProjectFilter
 
 class ProjectList(generics.ListAPIView):
+    """
+    class to display list of projects and option to filter
+    by technology name
+    """
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     # Use the filter set created
     filterset_class = ProjectFilter
     # Add the filter backend
     filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+
+
+class ProjectDetail(generics.RetrieveAPIView):
+    """
+    class to retrieve single project by id
+    """
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
